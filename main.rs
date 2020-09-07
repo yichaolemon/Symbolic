9d60074dbfdfd077cd99c041ab789620b3beebc5
Lee Danilek <lee.danilek@gmail.edu>
2020-09-07 16:17:19 -0700

    parser types

--- main.rs
+++ main.rs
@@ -1,6 +1,14 @@
 mod parser;
 
+use std::io;
+
 fn main() {
+    let mut expr = String::new();
+
+    io::stdin()
+        .read_line(&mut expr)
+        .expect("Failed to read expression");
+
     println!("Hello world!");
-    parser::parse();
+    parser::parse(expr);
 }
--- parser.rs
+++ parser.rs
@@ -1,3 +1,15 @@
-pub fn parse() {
-    println!("parser called");
+pub enum Expression {
+  Sum(Box<Expression>, Box<Expression>),
+  Product(Box<Expression>, Box<Expression>),
+  Constant(f64),
+  Variable(String),
+}
+
+pub struct ParseError {
+  input: String,
+}
+
+pub fn parse(expr: String) -> Result<Expression, ParseError> {
+  println!("parser called");
+  Ok(Expression::Constant(0.0))
 }
